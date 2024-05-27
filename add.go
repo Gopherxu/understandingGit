511c46fcf6239194c1109c5e8b49500fab18da08
Gopherxu <kartikshinde991@gmail.com>
2024-05-27 10:45:49 +0530

    Go Code for addition with Function

--- add.go
+++ add.go
@@ -1,11 +1,14 @@
 package main
 import "fmt"
+func Add(num1 int, num2 int){
+	var sum int
+	sum = num1 + num2
+	fmt.Println(sum)
+}
 func main(){
 	
 	var number1 int = 10
 	var number2 int = 5
-	var sum int
-	sum = number1 + number2
-	fmt.Println(sum)
+	Add(number1,number2)
 
 }
